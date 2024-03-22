0e44197b2c2545c11ec8282005900853a1d48d88
Ones <minhquang2002nt@gmail.com>
2024-03-22 11:25:33 +0700

    update

---
+++ Lap 5/footer.php
@@ -0,0 +1,9 @@
+    <div id="wrapper" class="container">
+        <footer>
+            Comeback <a href="/Lap 3/demo_lap3.sql">Home page</a><br>
+            &#169; 2022 - Trendemy
+        </footer>
+    </div>
+</body>
+
+</html>
---
+++ Lap 6/category.class.php
@@ -0,0 +1,10 @@
+<?php
+    require_once('db.class.php');
+
+    class Category{
+        public $cateID;
+        public $categoryName;
+        public $description;
+        
+    }
+?>
